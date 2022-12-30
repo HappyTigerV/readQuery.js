e021ec4002bece3749eef6d6fd960a11029cd6bb
HappyTigerV <1874855047@qq.com>
2022-12-30 18:16:24 +0800

    V3

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 # readQuery.js
 This repo allows you to read the parameters after "?" in the href without using the server (pure client)!
-## Usage 
-Execute query().It will return a object of the parmeters after "?" in the href.
+## Download 
+Download `readQuery.js` or `readQuery.min.js`, and add a `<script>` tag to your HTML document like this:
+```html
+<script src="./readQuery.js"></script>
+```
+## API
+### `window.location.query` 
+`window.location.query` is an object.
+For example, if this is the query string:
+```
+?a=hello&b=world
+```
+The `window.location.query` should be like this:
+```javascript
+{
+    a: "hello",
+    b: "world"
+}
+```
+### `window.location.queryString` 
+`window.location.queryString` is the query string.
+
+## Examples
+Check examples [here](example/).
--- src/readQuery.js
+++ src/readQuery.js
@@ -1,25 +1,26 @@
 /*
 https://github.com/HappyTigerV/readQuery.js
-V2.1 Gold 
+V3.0 Gold 
+Followed MPL V2
 */
-function query(){
-	let query_obj={};
-	let query_string=getQueryString();
-	if(query_string){
-		let arr=query_string.split("&");
-		for(let i of arr){
-			query_obj[i.split("=")[0]]=i.split("=")[1];
+
+window.location.queryString = (function getQueryString() {
+	let str_length = (window.location.protocol + "/\/" + window.location.host + window.location.pathname).length;
+	let query_string = window.location.href.slice(str_length);
+	if (query_string && query_string.slice(0, 1) == "?") {
+		query_string = query_string.slice(1);
+	}
+	else query_string = "";
+	return query_string;
+})();
+
+window.location.query = (function () {
+	let query_obj = {};
+	if (window.location.queryString) {
+		let arr = window.location.queryString.split("&");
+		for (let i of arr) {
+			query_obj[i.split("=")[0]] = i.split("=")[1];
 		}
 	}
 	return query_obj;
-}
-
-function getQueryString(){
-	let str_length=(window.location.protocol+"\/\/"+window.location.host+window.location.pathname).length;
-	let query_string=window.location.href.slice(str_length);
-	if(query_string && query_string.slice(0,1)=="?"){
-		query_string=query_string.slice(1);
-	}
-	else query_string="";
-	return query_string;
-}
+})();
