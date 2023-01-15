9e210166e2235e5302ca03fc263e877e90ea2500
HappyTigerV <1874855047@qq.com>
2023-01-15 14:55:38 +0800

    V3.1

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # readQuery.js
 This repo allows you to read the parameters after "?" in the href without using the server (pure client)!
-## Download 
+## Usage 
 Download `readQuery.js` or `readQuery.min.js`, and add a `<script>` tag to your HTML document like this:
 ```html
-<script src="./readQuery.js"></script>
+<script src="/path/to/readQuery.js"></script>
 ```
 ## API
 ### `window.location.query` 
 `window.location.query` is an object.
-For example, if this is the query string:
+For example, if this is the link:
 ```
-?a=hello&b=world
+https://your-site.com/?a=hello&b=world
 ```
 The `window.location.query` should be like this:
 ```javascript
@@ -19,8 +19,29 @@ The `window.location.query` should be like this:
     b: "world"
 }
 ```
+#### New feature(s) in V3.1
+You can also set the query object like this:
+```javascript
+window.location.query = {a: "hello"};
+```
+After running the above code, you should be redirected to this page:
+```
+https://your-site.com/?a=hello
+```
 ### `window.location.queryString` 
-`window.location.queryString` is the query string.
-
+`window.location.queryString` is the query string.  
+For example:
+```
+a=hello&b=world
+```
+#### New feature(s) in V3.1
+You can also set the query string like this:
+```javascript
+window.location.queryString = "a=hello";
+```
+After running the above code, you should be redirected to this page:
+```
+https://your-site.com/?a=hello
+```
 ## Examples
 Check examples [here](examples/).
--- src/readQuery.js
+++ src/readQuery.js
@@ -1,26 +1,55 @@
-/*
-https://github.com/HappyTigerV/readQuery.js
-V3.0 Gold 
-Followed MPL V2
-*/
+/*!
+ * readQuery.js
+ * (c) 2023 @HappyTigerV
+ * https://github.com/HappyTigerV/readQuery.js
+ * V3.1 Gold
+ * Released under the Mozilla Public License V2.0.
+ */
 
-window.location.queryString = (function getQueryString() {
-	let str_length = (window.location.protocol + "/\/" + window.location.host + window.location.pathname).length;
-	let query_string = window.location.href.slice(str_length);
-	if (query_string && query_string.slice(0, 1) == "?") {
-		query_string = query_string.slice(1);
-	}
-	else query_string = "";
-	return query_string;
-})();
-
-window.location.query = (function () {
-	let query_obj = {};
-	if (window.location.queryString) {
-		let arr = window.location.queryString.split("&");
-		for (let i of arr) {
-			query_obj[i.split("=")[0]] = i.split("=")[1];
+Object.defineProperty(window.location, "queryString", {
+	/**
+	 * @returns {string}
+	 */
+	get() {
+		let str_length = (window.location.protocol + "/\/" + window.location.host + window.location.pathname).length;
+		let query_string = window.location.href.slice(str_length);
+		if (query_string && query_string.slice(0, 1) == "?") {
+			query_string = query_string.slice(1);
 		}
+		else query_string = "";
+		return query_string;
+	},
+	/**
+	 * @param {string} v 
+	 */
+	set(v) {
+		window.location.href = window.location.protocol + "/\/" + window.location.host + window.location.pathname + "?" + v;
 	}
-	return query_obj;
-})();
+});
+
+Object.defineProperty(window.location, "query", {
+	/**
+	 * @returns {string}
+	 */
+	get() {
+		let query_obj = {};
+		if (window.location.queryString) {
+			let arr = window.location.queryString.split("&");
+			for (let i of arr) {
+				query_obj[i.split("=")[0]] = i.split("=")[1];
+			}
+		}
+		return query_obj;
+	},
+	/** 
+	 * @param {object} v 
+	 */
+	set(v) {
+		let queryString = "";
+		for (let i in v) {
+			queryString += `&${i}=${v[i]}`;
+		}
+		queryString = queryString.slice(1, queryString.length);
+		window.location.queryString = queryString;
+	}
+});
